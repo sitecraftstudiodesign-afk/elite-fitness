ec4b1164c1d42b5fc4f068d572b5597301ac0bb1
sitecraftstudiodesign-afk <sitecraftstudiodesign@gmail.com>
2026-06-22 21:46:15 +0100

    Update back to SiteCraft link

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -51,7 +51,7 @@ function Navbar() {
       </div>
 
       <a
-        href="http://localhost:5173#projects"
+        href="https://sitecraft-studio-liart.vercel.app#projects"
         onMouseEnter={() => setHovered(true)}
         onMouseLeave={() => {
           setHovered(false);
